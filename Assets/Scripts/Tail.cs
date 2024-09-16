25f8c585aa30acf84d2589c10232334d4209525f
Alexandre Rocha Coutinho <alex.rochacoutinho@gmail.com>
2024-09-16 23:28:54 +0200

    Tail Flickering Fixed

--- Assets/Scripts/Tail.cs
+++ Assets/Scripts/Tail.cs
@@ -9,6 +9,7 @@ public class Tail : MonoBehaviour
     [SerializeField] float distance = 0.3f;
     [SerializeField] float moveStep = 0.3f;
     [SerializeField] Collider _collider;
+    float stopMovingDistance;
 
     private Vector3 _targetPosition;
 
@@ -18,15 +19,24 @@ public class Tail : MonoBehaviour
 
     private void Start() {
         _collider ??= GetComponent<Collider>() ?? GetComponentInChildren<Collider>();
+        stopMovingDistance = distance * 0.80f;
     }
     private void Update() {
-        if (followTransform == null) {
+        if (followTransform == null || (transform.position - followTransform.position).magnitude <= stopMovingDistance) {
             return;
         }
-
         _targetPosition = followTransform.position - followTransform.forward * distance;
-        _targetPosition += (transform.position - _targetPosition) * delayTime;
+        _targetPosition += (transform.position - _targetPosition) * delayTime; 
         transform.position = Vector3.Lerp(transform.position, _targetPosition, Time.deltaTime * moveStep);
-        transform.LookAt(_targetPosition);
+        RotateToTarget(_targetPosition);
+    }
+
+    private void RotateToTarget(Vector3 targetPosition) {
+        transform.LookAt(targetPosition);
+        var currentRotation = transform.rotation;
+        currentRotation.x = 0f;
+        currentRotation.z = 0f;
+        transform.rotation = currentRotation;
+
     }
 }
